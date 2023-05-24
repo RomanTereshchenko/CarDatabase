46f765bf89745393fe87a28737e24f5212bf2264
Asus <Asus@DESKTOP-5NL9HPH>
2023-05-24 20:40:03 +0300

    Initial commit

---
+++ car-db/src/test/java/com/foxminded/javaspring/cardb/CarDbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.foxminded.javaspring.cardb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarDbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
