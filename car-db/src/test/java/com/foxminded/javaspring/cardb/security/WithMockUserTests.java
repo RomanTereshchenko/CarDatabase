51d732648651eb88ea0f1a9c92937e66e51abb36
Asus <Asus@DESKTOP-5NL9HPH>
2023-06-21 20:22:32 +0300

    Submitting Task 4.2 Create RestApi endpoints (v.3)

--- car-db/src/test/java/com/foxminded/javaspring/cardb/security/WithMockUserTests.java
+++ car-db/src/test/java/com/foxminded/javaspring/cardb/security/WithMockUserTests.java
@@ -1,4 +1,4 @@
-package com.foxminded.javaspring.cardb.securityTests;
+package com.foxminded.javaspring.cardb.security;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
